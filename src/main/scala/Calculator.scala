1d7f44e3eb3b3413797f4e0ada37c6d75ede885f
waman <waman@coast.ocn.ne.jp>
2015-05-21 12:03:02 +0900

    Chapter 1 / Section 4

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -2,5 +2,8 @@ class Calculator {
 
   def multiply(x:Int, y:Int):Int = x * y
 
-  def divide(x:Int, y:Int):Int = x / y
+  def divide(x:Int, y:Int):Float = y match{
+    case 0 => throw new IllegalArgumentException("divide by zero.")
+    case _ => x.toFloat / y.toFloat
+  }
 }
--- src/test/scala/CalculatorSpec.scala
+++ src/test/scala/CalculatorSpec.scala
@@ -2,7 +2,32 @@ import org.scalatest.FlatSpec
 
 class CalculatorSpec extends FlatSpec{
 
-  "A Calculator" should "do something" in {
-    fail("まだ実装されていません")
+  "Calculator" should "multiplyで3と4の乗算結果が取得できる" in {
+    val calc = new Calculator
+    val expected = 12
+    val actual = calc.multiply(3, 4)
+    assert(actual == expected)
+  }
+
+  it should "multiplyで5と7の乗算結果が取得できる" in {
+    val calc = new Calculator
+    //val expected = 12
+    val expected = 35
+    val actual = calc.multiply(5, 7)
+    assert(actual == expected)
+  }
+
+  it should "divideで3と2の除算結果が取得できる" in {
+    val calc = new Calculator
+    val expected = 1.5f
+    val actual = calc.divide(3, 2)
+    assert(actual == expected)
+  }
+
+  it should "divideで5と0のときIllegalArgumentExceptionを送出する" in {
+    val calc = new Calculator
+    intercept[IllegalArgumentException]{
+      calc.divide(5, 0)
+    }
   }
 }
