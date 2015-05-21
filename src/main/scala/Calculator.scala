8f1a544e09894d1ce0858879b6e3353075db9154
waman <waman@coast.ocn.ne.jp>
2015-05-21 11:34:28 +0900

    Chapter 1 / Section 3

---
+++ src/main/scala/Calculator.scala
@@ -0,0 +1,6 @@
+class Calculator {
+
+  def multiply(x:Int, y:Int):Int = x * y
+
+  def divide(x:Int, y:Int):Int = x / y
+}
---
+++ src/test/scala/CalculatorSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FlatSpec
+
+class CalculatorSpec extends FlatSpec{
+
+  "A Calculator" should "do something" in {
+    fail("まだ実装されていません")
+  }
+}
