2576f67aaa17c846816569f9a00ea6f52af2c1f9
waman <waman@coast.ocn.ne.jp>
2015-06-10 09:09:48 +0900

    Add ListBufferSpec

---
+++ src/test/scala/scalatest/tutorial/ListBufferSpec.scala
@@ -0,0 +1,30 @@
+package scalatest.tutorial
+
+import scala.collection.mutable
+import org.scalatest.{GivenWhenThen, FlatSpec}
+
+class ListBufferSpec extends FlatSpec with GivenWhenThen{
+
+  "ListBuffer" should "要素が2つ追加された状態で要素をremoveするとsizeが1となる" in {
+    // SetUp
+    Given("事前準備")
+//    val sut = mutable.ListBuffer.empty[String]
+//    note("ミュータブルだよ")
+//    sut += "Hello"
+//    sut += "World"
+    val sut = mutable.ListBuffer("Hello", "World")
+
+    // Exercise
+    When("実行")
+    sut.remove(0)
+
+    // Verify
+    Then("検証")
+    assert(sut.size == 1)
+    assert(sut.head == "World")
+
+    // tearDown
+    info("後処理")
+    alert("警告があったらalert")
+  }
+}
